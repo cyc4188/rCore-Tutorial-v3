52b0239ef5ee9f6907add9b8b98be5c5b1386e52
cyc4188 <1065768794@qq.com>
2023-11-16 13:15:44 +0800

    ch2-lab: pass

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,15 +1,27 @@
+use crate::batch::{APP_BASE_ADDRESS, APP_SIZE_LIMIT, USER_STACK};
+const USER_STACK_SIZE: usize = 4096;
 const FD_STDOUT: usize = 1;
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
+            // check if buf is valid
+            if (buf as usize >= USER_STACK.get_sp() - USER_STACK_SIZE
+                && buf as usize + len <= USER_STACK.get_sp())
+                || (buf as usize >= APP_BASE_ADDRESS
+                    && buf as usize + len <= APP_BASE_ADDRESS + APP_SIZE_LIMIT)
+            {
+                return -1;
+            }
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
             let str = core::str::from_utf8(slice).unwrap();
             print!("{}", str);
             len as isize
-        },
+        }
         _ => {
-            panic!("Unsupported fd in sys_write!");
+            -1
+            // panic!("Unsupported fd in sys_write!");
         }
     }
 }
+
